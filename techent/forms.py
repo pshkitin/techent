58f57e6f8cdfb584d3c153fc494007788489d11c
Anastasia Latynskaya <anlatunskaya@gmail.com>
2012-07-17 12:54:13 +0400

    Make events to save into database

--- techent/forms.py
+++ techent/forms.py
@@ -6,6 +6,7 @@ from techent.models import Event
 from mongoengine.queryset import DoesNotExist
 from isodate import parse_datetime
 from isodate.isoerror import ISO8601Error
+import datetime
 
 class EventForm(Form):
     subject = TextField("Subject", [validators.Required()])
@@ -15,34 +16,28 @@ class EventForm(Form):
     description = TextField("Description")
     location = SelectField('City', choices=[('moscow', 'Moscow'), ('saratov', 'Saratov'), ('st_petersburg', 'St.Petersburg'), ('usa_city','USA City')])
     hosts = TextField("Hosts description")
-    domain = TextField("Domain", [validators.Required(), validators.URL()])
     tags = TextField("Tags")
 
     def validate_start_date(self, field):
-        self.iso_date_validation(field.data)
+        self.iso_date(field.data)
 
     def validate_end_date(self, field):
-        end_dt = self.iso_date_validation(field.data)
+        end_dt = self.iso_date(field.data)
         # Compare date
         # Validate start date because of nobody can garantie the order of field validation
         try:
-            start_dt = self.iso_date_validation(self.start_date.data)
+            start_dt = self.iso_date(self.start_date.data)
         except ValidationError:
             return
         if start_dt > end_dt:
             raise ValidationError("End date should be less then start date")
 
-    def validate_domain(self, field):
-        try:
-            event = Event.objects.get(domain = field.data)
-        except DoesNotExist:
-            return
-        raise ValidationError("Domain isn't available")
-
-    def iso_date_validation(self, date_string):
+    def iso_date(self, date_string):
         try:
             # TODO: replase this parser to more strict, because this parser can parse
             # this 2008-09hhkjhkjhjk-03T20:56:35.asdads450686+00:01
-            return parse_datetime(date_string)
+            date = datetime.datetime.strptime(date_string, "%d.%m.%Y @ %H:%M")
+            return date
         except (ValueError, ISO8601Error):
             raise ValidationError("Date is in invalid format")
+
